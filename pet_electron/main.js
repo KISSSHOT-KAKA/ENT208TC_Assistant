41911233fbb7a4978d5d102ef74faa9c44443147
KISSSHOT-KAKA <yepnope811@gmail.com>
2026-04-24 23:46:45 +0800

    feat: import Windows Electron UI and ws client

---
+++ pet_electron/main.js
@@ -0,0 +1,24 @@
+﻿const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 980,
+    height: 680,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
